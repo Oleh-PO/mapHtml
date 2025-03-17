81bbf207c006dac6d24e9ff1e936844cc6928994
unknown <oleh.po.2007@gmail.com>
2025-03-17 22:18:27 +0200

    17.03.2025

---
+++ json.js
@@ -0,0 +1,24 @@
+var	json = 
+[
+{
+	"name": "testqwertyuikjhgfdfvbnj",
+	"x":20,
+	"y": 5,
+	"type": "standart",
+	"context": "LOREMIPSUM",
+},
+{
+	"name": "test",
+	"x":80,
+	"y": 5,
+	"type": "standart",
+	"context": "LOREMIPSUM",
+},
+{
+	"name": "test",
+	"x":5,
+	"y": 80,
+	"type": "standart",
+	"context": "LOREMIPSUM",
+}
+]
--- mapEngin.js
+++ mapEngin.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     document.querySelector(".toggle").addEventListener("click", function (event) {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-        return;
-    }
-    document.querySelector(".screen").requestFullscreen();
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+            return;
+        }
+        document.querySelector(".main").requestFullscreen();
     });
+    fetch('mTEST.json', {mode: 'no-cors'})
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 });
